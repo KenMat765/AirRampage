ed36940396b092a87cda0ff28c5be559005a2eb5
KentoM756 <kent3matsu@gmail.com>
2024-06-10 22:47:36 +0900

    Recovered AbilityFactory

--- Assets/Scripts/Manager/PlayerInfo.cs
+++ Assets/Scripts/Manager/PlayerInfo.cs
@@ -151,12 +151,12 @@ public class PlayerInfo
         false, false, false, false, false, false,
         false, false, false, false, false, false,
         false, false, false, false, false, false,
-        true, true, true, true, true, true,
-        true, true, true, true, true, true,
-        true, true, true, true, true, true,
-        true, true, true, true, true, true,
-        true, true, true, true, true, true,
-        true, true, true, true, true, true,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
+        false, false, false, false, false, false,
     };
 
     public bool[] abi_equip =
