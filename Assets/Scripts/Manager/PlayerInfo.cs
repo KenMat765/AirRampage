2846b1a1c65d9135cbc6d6cfcfa9dbe7a46cb2e2
KentoM756 <kent3matsu@gmail.com>
2024-06-16 02:35:42 +0900

    Modified sprite format

--- Assets/Scripts/Manager/PlayerInfo.cs
+++ Assets/Scripts/Manager/PlayerInfo.cs
@@ -148,7 +148,7 @@ public class PlayerInfo
 
     public bool[] abi_unlock =
     {
-        false, false, false, false, false, false,
+        true, true, true, true, true, true,
         false, false, false, false, false, false,
         false, false, false, false, false, false,
         false, false, false, false, false, false,
