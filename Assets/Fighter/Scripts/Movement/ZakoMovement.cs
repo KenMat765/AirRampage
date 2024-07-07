72fc54789cf66b7f0aaf115b54bea330868a0cbd
KentoM756 <kent3matsu@gmail.com>
2024-07-07 23:10:16 +0900

    Added NetworkTransform to Cannon

--- Assets/Fighter/Scripts/Movement/ZakoMovement.cs
+++ Assets/Fighter/Scripts/Movement/ZakoMovement.cs
@@ -5,32 +5,30 @@ using UnityEngine;
 
 public class ZakoMovement : Movement
 {
-    ZakoCondition zakoCondition;
-    ZakoAttack zakoAttack;
+    Transform trans;
     public Transform array_point { get; set; }
 
     protected override void Awake()
     {
         base.Awake();
         rotationSpeed = 1.5f;
-        zakoCondition = (ZakoCondition)fighterCondition;
-        zakoAttack = (ZakoAttack)fighterCondition.attack;
+        trans = transform;
     }
 
     protected override void FixedUpdate()
     {
-        if (BattleInfo.isMulti && !IsHost) return;
+        if (!IsHost) return;
         base.FixedUpdate();
     }
 
 
     protected override void Rotate()
     {
-        Vector3 diff_pos = array_point.position - transform.position;
-        Vector3 relativeEulerAngle = Quaternion.LookRotation(((diff_pos == Vector3.zero) ? transform.forward : diff_pos)).eulerAngles;
-        float relativeYAngle = Vector3.SignedAngle(transform.forward, diff_pos, Vector3.up);
+        Vector3 diff_pos = array_point.position - trans.position;
+        Vector3 relativeEulerAngle = Quaternion.LookRotation(diff_pos == Vector3.zero ? trans.forward : diff_pos).eulerAngles;
+        float relativeYAngle = Vector3.SignedAngle(trans.forward, diff_pos, Vector3.up);
         Quaternion lookRotation = Quaternion.Euler(relativeEulerAngle.x, relativeEulerAngle.y, Mathf.Clamp(-relativeYAngle * 1.5f, -maxTiltZ, maxTiltZ));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+        trans.rotation = Quaternion.Slerp(trans.rotation, lookRotation, rotationSpeed * Time.deltaTime);
     }
 
 
